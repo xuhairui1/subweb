b9d9ff07f7c2fad69fa4e407e83196b74ba44ac5
barry green <61643072+xuhairui1@users.noreply.github.com>
2023-04-05 16:56:17 +0800

    Update navBarItem.js

--- src/layouts/components/header/navBarItem.js
+++ src/layouts/components/header/navBarItem.js
@@ -4,14 +4,4 @@ export const navBarItem = [
     link: '/',
     target: '',
   },
-  {
-    title: '短链接',
-    link: 'https://s.ops.ci',
-    target: '_blank',
-  },
-  {
-    title: 'GitHub',
-    link: 'https://github.com/stilleshan/subweb',
-    target: '_blank',
-  },
 ];
